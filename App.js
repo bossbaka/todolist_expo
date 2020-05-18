1e15f06b3dd4cb95291d078e859557fe2c9389eb
Phisek <phisek@nexpie.com>
2020-05-18 22:46:14 +0700

    Part3 - Passing Props to the Todo Modal

--- App.js
+++ App.js
@@ -1,19 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "./Colors";
+import TempData from "./TempData";
+import TodoList from "./components/TodoList";
+import AddListModal from "./components/AddListModal";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    addTodoVisible: false,
+    lists: TempData,
+  };
+
+  toggleAddTodoModal() {
+    this.setState({ addTodoVisible: !this.state.addTodoVisible });
+  }
+
+  renderList = (list) => {
+    return <TodoList list={list} />;
+  };
+
+  addList = (list) => {
+    this.setState({
+      lists: [
+        ...this.state.lists,
+        { ...list, id: this.state.lists.length + 1, todos: [] },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          visible={this.state.addTodoVisible}
+          onRequestClose={() => this.toggleAddTodoModal()}
+        >
+          <AddListModal
+            closeModal={() => this.toggleAddTodoModal()}
+            addList={this.addList}
+          />
+        </Modal>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo{" "}
+            <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity
+            style={styles.addList}
+            onPress={() => this.toggleAddTodoModal()}
+          >
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+          <Text style={styles.add}>Add List</Text>
+        </View>
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={this.state.lists}
+            keyExtractor={(item) => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => this.renderList(item)}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontSize: 14,
+    fontWeight: "600",
+    marginTop: 8,
   },
 });
---
+++ TempData.js
@@ -0,0 +1,68 @@
+export default TempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: true,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Birthday Party",
+    color: "#2DD38D",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: false,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+];
